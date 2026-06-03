60eb9ac7250809b97bb7134befd84fb0a449ab17
r-near <163825889+r-near@users.noreply.github.com>
2026-06-03 07:29:22 -0700

    Update nearcore version to 2.12.0

--- src/lib.rs
+++ src/lib.rs
@@ -25,5 +25,5 @@ pub use config::{random_account_id, random_key_pair};
 
 // The current version of the sandbox node we want to point to.
 // Should be updated to the latest release of nearcore.
-// Currently pointing to nearcore@v2.11.1 released on April 21, 2026
-pub const DEFAULT_NEAR_SANDBOX_VERSION: &str = "2.11.1";
+// Currently pointing to nearcore@v2.12.0 (protocol 84) released on June 3, 2026
+pub const DEFAULT_NEAR_SANDBOX_VERSION: &str = "2.12.0";
