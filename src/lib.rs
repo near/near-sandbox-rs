3cdb4e4aaa1e2a3397bf39f717fd933c3f6a591e
r-near <163825889+r-near@users.noreply.github.com>
2026-09-16 11:06:48 -0700

    feat: default sandbox to nearcore 2.14.0-rc.1 (protocol 87)
    - DEFAULT_NEAR_SANDBOX_VERSION -> 2.14.0-rc.1
    - near-account-id 2 -> 3 (AccountId is part of the public API; nearcore 0.38
      consumers such as near-workspaces move to 3)
    - singleton_sandbox example: use near_api::AccountId where the id is handed
      to near-api, which still pins near-account-id 2

--- src/lib.rs
+++ src/lib.rs
@@ -25,4 +25,5 @@ pub use config::{random_account_id, random_key_pair};
 
 // The current version of the sandbox node we want to point to.
 // Should be updated to the latest release of nearcore.
-pub const DEFAULT_NEAR_SANDBOX_VERSION: &str = "2.13.4";
+// Currently pointing to nearcore@v2.14.0-rc.1 (protocol 87) released on September 16, 2026
+pub const DEFAULT_NEAR_SANDBOX_VERSION: &str = "2.14.0-rc.1";
